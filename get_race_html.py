39823adea1365d2cd55ff8d35667c6016d3225cd
unonao <naokiuno634@gmail.com>
2019-11-12 13:46:16 +0900

    change log

--- get_race_html.py
+++ get_race_html.py
@@ -12,11 +12,13 @@ OWN_FILE_NAME = path.splitext(path.basename(__file__))[0]
 RACR_URL_DIR = "race_url"
 RACR_HTML_DIR = "race_html"
 
+
 import logging
-logger = logging.getLogger(__name__) # get_race_html.py の名前を渡す
-formatter = '%(levelname)s : %(asctime)s : %(message)s' # フォーマットを定義
+formatter = "%(asctime)s [%(levelname)s]\t%(message)s" # フォーマットを定義
+#formatter_func = "%(asctime)s\t[%(levelname)8s]\t%(message)s from %(func)" # フォーマットを定義
 logging.basicConfig(filename='logfile/'+OWN_FILE_NAME+'.logger.log', level=logging.INFO, format=formatter)
-logging.info("start script...")
+logger = logging.getLogger(__name__) #ファイルの名前を渡す
+logger.info("start script...")
 
 
 def my_makedirs(path):
@@ -33,7 +35,7 @@ def get_race_html_by_year_and_mon(year,month):
         save_dir = RACR_HTML_DIR+"/"+str(year)+"/"+str(month)
         my_makedirs(save_dir)
         urls = f.read().splitlines()
-        print("getting "+str(len(urls))+ " htmls in",year,month,)
+        logger.info("getting htmls ("+str(year) +" "+ str(month) + ")")
         for url in urls:
             list = url.split("/")
             race_id = list[-2]
@@ -45,7 +47,7 @@ def get_race_html_by_year_and_mon(year,month):
                 time.sleep(1)
                 with open(save_file_path, 'w') as file:
                     file.write(html)
-    logging.info("save " + str(len(urls)) +" htmls to "+save_dir)
+    logging.info("saved " + str(len(urls)) +" htmls ("+str(year) +" "+ str(month) + ")")
 
 if __name__ == '__main__':
     print("start get race html!")
